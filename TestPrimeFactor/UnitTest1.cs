e063eaff41316790bba959a735f75a72355f3e6f
Ray Huang <eeyellow@gmail.com>
2016-04-16 20:09:16 +0800

    test: add new test case TestMethod3
    TestMethod3 input parameter 3 and except the ouput is 3

--- TestPrimeFactor/UnitTest1.cs
+++ TestPrimeFactor/UnitTest1.cs
@@ -7,9 +7,15 @@ namespace VIVALACODE
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod2()
         {
             Assert.AreEqual(2, PrimeFactor.breakdown(2));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(3, PrimeFactor.breakdown(3));
         }
     }
 }
